f7e0383cf2b0f4f1921abd3a81d4f468c9a55d8d
SaurabhSurolia <168442537+SaurabhSurolia@users.noreply.github.com>
2024-04-29 18:43:36 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,59 @@
+function viewRacquet() {
+    let x = document.getElementById('rackquet-2');
+    let y = document.getElementById('view-button-racq');
+    if(x.style.display === "none") {
+        x.style.display = "flex";
+        y.innerHTML = "View Less";
+    } else {
+        x.style.display = "none";
+        y.innerHTML = "View More";
+    }
+}
+
+function viewShoe() {
+    let x = document.getElementById('Shoe-2');
+    let y = document.getElementById('view-button-shoe');
+    if(x.style.display === "none") {
+        x.style.display = "flex";
+        y.innerHTML = "View Less";
+    } else {
+        x.style.display = "none";
+        y.innerHTML = "View More";
+    }
+}
+
+function viewKit() {
+    let x = document.getElementById('Kitbag-2');
+    let y = document.getElementById('view-button-kit');
+    if(x.style.display === "none") {
+        x.style.display = "flex";
+        y.innerHTML = "View Less";
+    } else {
+        x.style.display = "none";
+        y.innerHTML = "View More";
+    }
+}
+
+function viewJersey() {
+    let x = document.getElementById('Jersey-2');
+    let y = document.getElementById('view-button-jer');
+    if(x.style.display === "none") {
+        x.style.display = "flex";
+        y.innerHTML = "View Less";
+    } else {
+        x.style.display = "none";
+        y.innerHTML = "View More";
+    }
+}
+
+function viewShuttle() {
+    let x = document.getElementById('Shuttle-2');
+    let y = document.getElementById('view-button-shut');
+    if(x.style.display === "none") {
+        x.style.display = "flex";
+        y.innerHTML = "View Less";
+    } else {
+        x.style.display = "none";
+        y.innerHTML = "View More";
+    }
+}
